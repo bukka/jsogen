3456d8f9eb0a764441059a13b7369ffb93501114
Jakub Zelenka <bukka@php.net>
2014-07-14 20:39:52 +0100

    Parse template arg

--- jsogen/jsogen.py
+++ jsogen/jsogen.py
@@ -4,11 +4,18 @@ import sys
 import argparse
 
 def main(argv=False):
-    print 'JSOGen'
-    
     argv = (argv or sys.argv)[1:]
     
+    parser = argparse.ArgumentParser(description='JavaScript Object Generator')
+
+    parser.add_argument("template")
+
+    args = parser.parse_args(argv)
+
+    print(args.template)
+
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
