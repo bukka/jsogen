ac0c8efc932b3979177792ffddfa5f34ef599485
Jakub Zelenka <bukka@php.net>
2014-07-27 16:50:56 +0100

    Tidy up arguments

--- jsogen/jsogen.py
+++ jsogen/jsogen.py
@@ -5,14 +5,17 @@ import argparse
 
 def main(argv=False):
     argv = (argv or sys.argv)[1:]
-    
+
     parser = argparse.ArgumentParser(description='JavaScript Object Generator')
 
-    parser.add_argument("template")
+    parser.add_argument('template',
+                        help='template file or directory')
+    parser.add_argument('-s', '--seed', type=int,
+                        help='random generator seed value')
 
     args = parser.parse_args(argv)
 
-    print(args.template)
+    print(args)
 
     return 0
 
