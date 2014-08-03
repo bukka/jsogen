666004d3fa44dd6031db3d0e0ef0a1061fb31721
Jakub Zelenka <bukka@php.net>
2014-08-03 17:32:03 +0100

    Add template validation

--- jsogen/jsogen.py
+++ jsogen/jsogen.py
@@ -3,17 +3,38 @@ import os
 import sys
 import argparse
 
+class TemplateAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        path = values
+        if os.path.isfile(path):
+            self.checkReadAcces(path, 'file')
+            setattr(namespace, self.dest, values)
+        elif os.path.isdir(path):
+            self.checkReadAcces(path, 'directory')
+            setattr(namespace, self.dest, values)
+        else:
+            raise argparse.ArgumentTypeError("Template path '%s' is neither file nor directory" % path)
+
+    def checkReadAcces(self, path, what):
+        if not os.access(path, os.R_OK):
+            raise argparse.ArgumentTypeError("Template %s path '%s' is not readable" % (what, path))
+
+
 def main(argv=False):
     argv = (argv or sys.argv)[1:]
 
     parser = argparse.ArgumentParser(description='JavaScript Object Generator')
 
-    parser.add_argument('template',
+    parser.add_argument('template', action=TemplateAction,
                         help='template file or directory')
     parser.add_argument('-s', '--seed', type=int,
                         help='random generator seed value')
 
-    args = parser.parse_args(argv)
+    try:
+        args = parser.parse_args(argv)
+    except argparse.ArgumentTypeError as exc:
+        print(exc)
+        return -1
 
     print(args)
 
