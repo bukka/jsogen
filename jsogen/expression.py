120ace9a389571ddabe641f51d4c7bd91967d196
Jakub Zelenka <bukka@php.net>
2014-08-26 20:45:10 +0100

    Add initial expression scanner

--- jsogen/expression.py
+++ jsogen/expression.py
@@ -1,3 +1,4 @@
+import string
 
 class Expression:
     """Expression class"""
@@ -10,3 +11,35 @@ class Expression:
 
     def run(self):
         self.output.write(self.es)
+
+
+class Token:
+    """Tokens"""
+
+    t_empty = 1
+    t_int = 2
+    t_float = 3
+    t_string = 4
+
+
+class Scanner:
+    """Expression Scanner"""
+
+    def __init__(self, es):
+        self.es = es
+        self.start = 0
+        self.pos = 0
+        self.value = None
+        self.token = None
+
+    def scan(self):
+        es = self.es
+        if (es[self.start] in string.whitespace):
+            self.start += 1
+        p = self.start
+        in_string = False
+        token = Token.t_empty
+        while p < es.len:
+            if not in_string and es[p] in string.whitespace:
+                return token
+
